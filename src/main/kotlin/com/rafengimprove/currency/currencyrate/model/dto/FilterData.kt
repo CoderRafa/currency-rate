eb9d2640908335a25008c3d7b6692b1b883afaae
k.iakimchuk <k.yakimchuk@ventra.ru>
2025-03-02 11:43:41 +0300

    Change a field name.

--- src/main/kotlin/com/rafengimprove/currency/currencyrate/model/dto/FilterData.kt
+++ src/main/kotlin/com/rafengimprove/currency/currencyrate/model/dto/FilterData.kt
@@ -2,4 +2,4 @@ package com.rafengimprove.currency.currencyrate.model.dto
 
 import java.time.LocalDateTime
 
-data class FilterData(val lastName: String? = null, val beginDate: LocalDateTime? = null, val endDate: LocalDateTime? = null)
+data class FilterData(val text: String? = null, val beginDate: LocalDateTime? = null, val endDate: LocalDateTime? = null)
